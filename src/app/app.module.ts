ce18ff070c894dacc7b757a299d19d0f6fc5aa3d
Hulford <moraleshulford@gmail.com>
2023-11-24 23:48:54 -0500

    Creacion de componentes - Pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
